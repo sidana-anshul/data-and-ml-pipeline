162b3f6c04fb28db0429d0c63ead40d33efb419f
anshulsidana <sidana.anshul@gmail.com>
2022-05-19 12:47:10 +0530

    few redundant files were deleted

--- impala_queries.sql
+++ impala_queries.sql
@@ -1,15 +1,15 @@
 use anshulalab;
 
-1.
+--1.
 
 select t1.emp_no, t1.first_name, t1.last_name, t1.sex, t2.salary
 from employees_cap t1
-join salaries_cap t2 on t1.emp_no = t2.emp_no;
+join salaries_cap t2 on t1.emp_no = t2.emp_no limit 50;
 
 
 select first_name, last_name, hire_date
 from employees_cap
-where year(hire_date) = 1986;
+where year(hire_date) = 1986 limit 50;
 
 select count(*)
 from employees_cap
@@ -17,9 +17,9 @@ where year(hire_date) = 1986;
 
 
 
-2.A list showing the manager of each department with the 
-following information: department number, department name, 
-the manager's employee number, last name, first name.
+--2.A list showing the manager of each department with the 
+--following information: department number, department name, 
+--the manager's employee number, last name, first name.
 
 
 
@@ -29,46 +29,46 @@ from dept_manager_cap t1
 join departments_cap t2 on t1.dept_no = t2.dept_no
 join employees_cap t3 on t1.emp_no = t3.emp_no;
 
-3.
+--3.
 
 select t1.emp_no, t1.first_name, t1.last_name, t3.dept_name
 from employees_cap t1
 join dept_emp_cap t2 on t1.emp_no = t2.emp_no
 join departments_cap t3 on t2.dept_no = t3.dept_no
-limit 100; 
+limit 50; 
 
 
-4.
+--4.
 
 select first_name, last_name, sex
 from employees_cap
-where first_name = 'Hercules' and regexp_like(last_name, '^B.*');
+where first_name = 'Hercules' and regexp_like(last_name, '^B.*') limit 100;
 
-5.
-
-select t1.emp_no, t1.first_name, t1.last_name, t3.dept_name
-from employees_cap t1
-join dept_emp_cap t2 on t1.emp_no = t2.emp_no
-join departments_cap t3 on t2.dept_no = t3.dept_no
-where t3.dept_name = 'Sales' limit 100;
-
-6.
+--5.
 
 select t1.emp_no, t1.first_name, t1.last_name, t3.dept_name
 from employees_cap t1
 join dept_emp_cap t2 on t1.emp_no = t2.emp_no
 join departments_cap t3 on t2.dept_no = t3.dept_no
-where t3.dept_name in ('Sales', 'development') limit 100;
+where t3.dept_name = 'Sales' limit 50;
 
-7.
+--6.
+
+select t1.emp_no, t1.first_name, t1.last_name, t3.dept_name
+from employees_cap t1
+join dept_emp_cap t2 on t1.emp_no = t2.emp_no
+join departments_cap t3 on t2.dept_no = t3.dept_no
+where t3.dept_name in ('Sales', 'development') limit 50;
+
+--7.
 
 select last_name, count(last_name) as count_last_name
 from employees_cap
 group by last_name
 order by count_last_name desc
-limit 100;
+limit 50;
 
-8.
+--8.
 
 select t1.salary_bracket, count(t1.salary_bracket) as count_salary
 from (
@@ -83,14 +83,8 @@ order by count_salary desc;
 
 
 
-select concat_ws('-', cast(salary as string), cast(salary+10000 as string))
-from salaries_cap;
 
-select salary, concat_ws('-', cast((floor(salary/10000)*10000) as string), cast((ceil(salary/10000)*10000) as string))
-from salaries_cap limit 100;
-
-
-9.
+--9.
 
 select t1.emp_title_id, avg(t2.salary) as avg_salary
 from employees_cap t1
@@ -98,17 +92,7 @@ join salaries_cap t2 on t1.emp_no = t2.emp_no
 group by t1.emp_title_id
 order by avg_salary desc;
 
-10.
-
-max(last_date) 2013-03-10
-
-select emp_no,
-case
-when left_company = 1 then datediff(last_date, hire_date)
-else datediff('2013-03-10', hire_date)
-end as tenure_in_days
-from employees_cap
-limit 100;
+--10.
 
 
 select t1.tenure_in_years, count(t1.tenure_in_years) as count_emp
@@ -123,20 +107,20 @@ group by t1.tenure_in_years
 order by count_emp desc;
 
 
-11. Number of male female employees in the company
+--11. Number of male female employees in the company
 
 select sex, count(sex)
 from employees_cap
 group by sex;
 
-12. Number of employees per department
+--12. Number of employees per department
 
 select t2.dept_name, count(t2.dept_name)
 from dept_emp_cap t1
 join departments_cap t2 on t1.dept_no = t2.dept_no
 group by t2.dept_name;
 
-13. Average salary by department
+--13. Average salary by department
 
 select t3.dept_name, round(avg(t1.salary),2) as avg_dept_salary
 from
@@ -147,7 +131,7 @@ group by t3.dept_name;
 
 
 
-14. average age of employees by sex
+--14. average age of employees by sex
 
 select t1.sex, round(avg(t1.age), 2) as avg_age
 from
@@ -155,7 +139,7 @@ from
 from employees_cap) as t1
 group by t1.sex;
 
-15. average salary of employees by sex
+--15. average salary of employees by sex
 
 select t3.sex, round(avg(t3.salary),2)
 from
